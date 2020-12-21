b7b2f9300e4c64adba5f85e9e26b2977afc064a5
Mauricio Rodríguez <mauriciorodricar@hotmail.com>
2020-12-21 11:26:03 -0600

    Updated entretenimiento UI

--- src/app/components/cmp-entretenimiento/cmp-entretenimiento.component.ts
+++ src/app/components/cmp-entretenimiento/cmp-entretenimiento.component.ts
@@ -13,6 +13,8 @@ export class EntretenimientoComponent implements OnInit {
     resultadoBusqueda: IntBusqueda[];
     //parametro de busqueda de la ruta Search
     busquedaQuery: string;
+    //isvisibleArray
+    isVisibleArray: boolean[];
 
   constructor(private ServicioBuscarService: SrvBusquedaService, config: NgbCarouselConfig) {
     config.interval = 5000;
@@ -20,15 +22,41 @@ export class EntretenimientoComponent implements OnInit {
     config.pauseOnHover = true;
     config.wrap = true;
     config.showNavigationIndicators = true;
+    this.isVisibleArray = [];
   }
   ngOnInit(): void {
     this.busquedaGit();
   }
 
-  busquedaGit = () => { 
+  activateHover($event){
+    this.isVisibleArray[$event.target.id] = true;
+  }
+  disableHover($event){
+    this.isVisibleArray[$event.target.id] = false;
+  }
+  disableAllHover(){
+    for (const desc in this.isVisibleArray) {
+      this.isVisibleArray[desc] = false;
+    }
+  }
+
+  redirect($event){
+    window.open(
+              this.resultadoBusqueda[$event.target.id].url, "_blank","noopener noreferrer");
+  }
+
+  busquedaGit = () => {
     this.ServicioBuscarService.busquedaFederada(["kudasai", "anmo"]).then((response) => {
       this.resultadoBusqueda = response;
       console.log(this.resultadoBusqueda);
+      for (let noticia = 5; noticia < this.resultadoBusqueda.length; noticia++) {
+        let resumen = this.resultadoBusqueda[noticia].description.split('.')[0]+'. '+this.resultadoBusqueda[noticia].description.split('.')[1];
+        if (resumen.length<250) {
+          resumen += this.resultadoBusqueda[noticia].description.split('.')[2];
+        }
+        resumen = resumen.split("Este articulo pertenece")[0];
+        this.resultadoBusqueda[noticia].description = resumen;
+      }
     }, (error) => {
       alert("Error: " + error.statusText)
     })
